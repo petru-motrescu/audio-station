7833936e830929cf893b2ea13d53bfa4586b2f14
Petru Motrescu <petru.motrescu@gmail.com>
2024-06-06 09:02:51 +0300

    Moderize track test

--- audio-station-lib/test/track-tests.cpp
+++ audio-station-lib/test/track-tests.cpp
@@ -9,15 +9,13 @@
 #include "test-suite.hpp"
 using namespace audiostation;
 
-constexpr double EPSILON = 0.000000000000001;
-
-void test_empty_track() {
-    Track track;
-    for (int i = 0; i < 100; i++) {
-        assert_equal(0, track.render(), EPSILON, "");
-    }
-}
-
 void audiostation::TestSuite::run_track_tests() {
-    test_empty_track();
+
+    test("Empty track", [] {
+        Track track;
+        for (int i = 0; i < 100; i++) {
+            assert_equal(0, track.render());
+        }
+    });
+    
 }
