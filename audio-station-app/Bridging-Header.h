6b18d28703c0e14e2da4e74f8cf01e1ad23bc831
Petru Motrescu <petru.motrescu@gmail.com>
2024-06-06 09:16:56 +0300

    App structure tweaks

--- audio-station-app/Bridging-Header.h
+++ audio-station-app/Bridging-Header.h
@@ -2,6 +2,6 @@
 #ifndef BRIDGING_HEADER_H
 #define BRIDGING_HEADER_H
 
-#include "../../audio-station-lib/inc/audio-station-c.h"
+#include "../audio-station-lib/inc/audio-station-c.h"
 
 #endif // BRIDGING_HEADER_H
